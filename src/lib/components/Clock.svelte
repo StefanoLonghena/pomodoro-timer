ad9173f7eddc9501fb263c46c6b96e505ff5cdc5
Stefano Longhena <stefanolonghena@Stefanos-MacBook-Air.local>
2025-01-30 18:12:52 +0100

    Basic Timer

---
+++ src/lib/components/Clock.svelte
@@ -0,0 +1,12 @@
+<script>
+	let play = $state(false);
+	let seconds = $state(0);
+	setInterval(() => {
+		if (play) {
+			seconds += 1;
+		}
+	}, 1000);
+</script>
+
+<h1>{seconds}</h1>
+<button onclick={play ? play=false : play=true}>{play ? "Pause" : "Play"}</button>
