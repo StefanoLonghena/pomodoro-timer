046be25bffd3bdf2a6a70f48393ea4abe4250047
Stefano Longhena <stefanolonghena@Stefanos-MacBook-Air.local>
2025-01-31 21:56:00 +0100

    Tailwindcss install

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,12 +1,13 @@
 <script>
 	let play = $state(false);
-	let seconds = $state(0);
+	let seconds = $state(1500);
+	let minutes = $derived(seconds/60)
 	setInterval(() => {
 		if (play) {
-			seconds += 1;
+			seconds -= 1;
 		}
 	}, 1000);
 </script>
 
-<h1>{seconds}</h1>
-<button onclick={play ? play=false : play=true}>{play ? "Pause" : "Play"}</button>
+<p class="text-3xl">{minutes >= 1 ? parseInt(minutes).toString().padStart(2, "0") + ":" : ""}{(seconds%60).toString().padStart(2, "0")}</p>
+<button class="px-5 py-1 rounded-2xl {play ? "bg-red-500" : "bg-green-500"}" onclick={() => play ? play=false : play=true}>{play ? "Pause" : "Play"}</button>
