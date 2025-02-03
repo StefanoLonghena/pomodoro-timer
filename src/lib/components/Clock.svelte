e3194e069e9fd3036429870d3624ec9516292fef
Stefano Longhena <stefano.longhena@gmail.com>
2025-02-03 20:36:32 +0100

    Added Cycles counter and reset button

--- src/lib/components/Clock.svelte
+++ src/lib/components/Clock.svelte
@@ -1,10 +1,14 @@
 <script>
 	let play = $state(false);
-	let seconds = $state(25*60);
+	let seconds = $state(5);
 	let minutes = $derived(seconds / 60);
 	let pause = false;
-	let progress = $state(0)
-	let fillAngle = $state(0)
+	let fillAngle = $state(0);
+	let cycles = $state(0);
+
+	let { reset, resetCallback } = $props();
+
+
 	setInterval(() => {
 		if (play) {
 			seconds -= 1;
@@ -12,34 +16,82 @@
 	}, 1000);
 
 	$effect(() => {
-		if(seconds===0) {
-			if(pause) {
-				seconds=25*60
-				pause=false;
+		if (seconds === 0) {
+			if (pause) {
+				seconds = 25 * 60;
+				pause = false;
 			} else {
-				seconds=5*60
-				pause=true;
+				seconds = 5 * 60;
+				cycles++;
+				pause = true;
 			}
 		}
 
-		progress = ((seconds / (pause ? 5*60 : 25*60)));
-		fillAngle = (1 - progress) * 360
-		console.log(progress)
-	})
+		if(reset) {
+			seconds = 25*60;
+			cycles = 0;
+			resetCallback();
+		}
 
-	
+		fillAngle = (1 - seconds / (pause ? 5 * 60 : 25 * 60)) * 360;
+	});
+
 </script>
 
 <div class="flex flex-col items-center h-full justify-around">
-	<div class=" flex flex-col items-center justify-center rounded-full p-[10px] w-[30vh] h-[30vh]" style="background: conic-gradient(from 0deg, #EF4444 0deg, #EF4444 {fillAngle}deg, #1F2937 {fillAngle}deg, #1F2937 360deg)">
-		<p class="text-[8vh] font-roboto text-center text-[#fdfdfd] tabular-nums bg-[#010101] w-full h-full rounded-full flex items-center justify-center">
-			{parseInt(minutes).toString().padStart(2, '0')}:{(seconds % 60)
-				.toString()
-				.padStart(2, '0')}
-		</p>
-	</div>
-		<button
-			class="px-[4vh] py-[1.5vw] rounded-3xl bg-[#3d3d3d] hover:cursor-pointer"
-			onclick={() => (play ? (play = false) : (play = true))}>{play ? 'Pause' : 'Play'}</button
+	<div
+		class=" flex flex-col items-center justify-center rounded-full p-[10px] w-[30vh] h-[30vh]"
+		style="background: conic-gradient(from 0deg, #EF4444 0deg, #EF4444 {fillAngle -
+			15}deg, #1F2937 {fillAngle}deg, #1F2937 360deg)"
+	>
+		<p
+			class="text-[8vh] font-roboto text-center text-[#fdfdfd] tabular-nums bg-[#010101] w-full h-full rounded-full flex items-center justify-center"
 		>
+			{parseInt(minutes).toString().padStart(2, '0')}:{(seconds % 60).toString().padStart(2, '0')}
+		</p>
+		<div class="absolute bottom-[20vh] left-1/2 transform -translate-x-1/2 flex">
+			<svg onclick={() => { cycles <= 4 ? cycles++ : True }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6 text-white">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+			  </svg>
+			{#each { length: 4 }, index}
+				{#if index < cycles}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						fill="currentColor"
+						class="size-6 text-red-500"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M4.5 7.5a3 3 0 0 1 3-3h9a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9Z"
+							clip-rule="evenodd"
+						/>
+					</svg>
+				{:else}
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="size-6 text-white"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M5.25 7.5A2.25 2.25 0 0 1 7.5 5.25h9a2.25 2.25 0 0 1 2.25 2.25v9a2.25 2.25 0 0 1-2.25 2.25h-9a2.25 2.25 0 0 1-2.25-2.25v-9Z"
+						/>
+					</svg>
+				{/if}
+			{/each}
+			<svg onclick={() => { cycles > 0 ? cycles-- : True }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="size-6 text-white">
+				<path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
+			  </svg>
+			  
+		</div>
+	</div>
+	<button
+		class="px-[4vh] py-[1.5vw] rounded-3xl bg-[#3d3d3d] hover:cursor-pointer"
+		onclick={() => (play ? (play = false) : (play = true))}>{play ? 'Pause' : 'Play'}</button
+	>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,35 @@
 <script>
 	import Clock from '$lib/components/Clock.svelte';
+	let reset = $state(false);
+
+	function triggerReset() {
+		reset = true;
+	}
+
+	function resetCallback() {
+		reset = false;
+	}
 </script>
 
-<div class="bg-[#010101] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 rounded-2xl w-[50vh] h-[50vh]">
-	<Clock />
+<button onclick={triggerReset} class="hover:cursor-pointer"
+aria-label="reset"
+	><svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke-width="1.5"
+		stroke="currentColor"
+		class="size-6"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+		/>
+	</svg>
+</button>
+<div
+	class="bg-[#010101] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-10 rounded-2xl w-[50vh] h-[50vh]"
+>
+	<Clock {reset} {resetCallback} />
 </div>
